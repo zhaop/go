6d89bd6d69589bb888f50af6c849cce16dd6c108
Patrick Zhao <zhaopatrick@gmail.com>
2019-08-31 01:19:22 +0200

    Implement "console mode" (use ./go.x -c, then run '?' for help)

--- main.c
+++ main.c
@@ -6,11 +6,219 @@
 #include "players/teresa.h"
 #include "utils.h"
 
-int console_main() {
-	wprintf(L"You're in the console. Congrats!\n");
+/*
+Console mode:
+No reply if success; prints error otherwise.
 
-	wprintf(L"Not implemented :(\n");
-	return 1;
+Interpreter errors:
+- !command
+- !syntax
+
+Commands supported:
+
+- ?
+  Print commands.
+
+- c
+  Reset game state.
+
+- k %f
+  Set komi to arg.
+  Errors:
+  - !invalid
+
+- p 1|2 %c%c
+  Play move %c%c as player 1|2. %c%c parseable by move_parse.
+  Errors:
+  - !player
+  - !move
+  - !illegal
+  - !play %d
+    %d is move_result ("error code")
+
+- g 1|2
+  Calculate a move for the next player & print it (%c%c).
+  Errors: 
+  - !player
+  - !play %d
+    %d is move_result ("error code")
+
+- p
+  Draw the current state.
+
+- q
+  Quit
+*/
+int console_main() {
+	wprintf(L"Go console mode\n");
+
+	state* st = state_create();
+
+	teresa_params teresap = {500000, 0.5, 1.1, NULL, NULL};
+	player teresa = {"genmove", &teresa_play, &teresa_observe, &teresap};
+
+	while (true) {
+		wprintf(L"> ");
+
+		int result;
+
+		char input[256];
+		fgets(input, 255, stdin);
+		input[255] = '\0';
+
+		char line[256];
+		result = sscanf(input, "%[^\n]%*c", line);
+		line[255] = '\0';
+
+		char command = line[0];
+
+		switch (command) {
+			case '\0':
+				continue;
+				break;
+			case '?':
+				wprintf(L"?       Display help\n");
+				wprintf(L"c       Reset the game state\n");
+				wprintf(L"d       Print a drawing of the current state\n");
+				wprintf(L"dd      Print debug infos on the current state\n");
+				wprintf(L"k 6.5   Set komi to 6.5\n");
+				wprintf(L"p 1 8b  Play move 8b as Black (player 1)\n");
+				wprintf(L"g 2     Calculate a move for White (player 2)\n");
+				wprintf(L"q       Quit\n");
+				break;
+			case 'c':
+			case 'q':
+			case 'v':
+				break;
+			case 'd':
+				if (line[1] != '\0' && line[1] != 'd') {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+				break;
+			case 'k':
+			case 'p':
+			case 'g':
+				if (line[1] != ' ') {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+				break;
+			default:
+				wprintf(L"!command\n");
+				continue;
+				break;
+		}
+
+		switch (command) {
+			case 'c': {
+				float komi = st->komi;
+				state_destroy(st);
+
+				st = state_create();
+				st->komi = komi;
+				break;
+			}
+			case 'd': {
+				if (line[1] == '\0') {
+					state_print(st);
+				} else if (line[1] == 'd') {
+					state_dump(st);
+				}
+				break;
+			}
+			case 'k': {
+				float komi;
+				result = sscanf(line + 2, "%f", &komi);
+
+				if (result != 1) {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+
+				st->komi = komi;
+				break;
+			}
+			case 'p': {
+				int player_in;
+				char mv_in[2];
+				char test;
+				result = sscanf(line + 2, "%d %c%c%c", &player_in, mv_in, mv_in + 1, &test);
+
+				if (result != 3) {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+
+				if (player_in != 1 && player_in != 2) {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+
+				color player = (player_in == 1) ? BLACK : WHITE;
+				if (player != st->nextPlayer) {
+					// TODO Violates GTP: color "should" not be constrained
+					wprintf(L"!player\n");
+					continue;
+				}
+
+				move mv;
+				if (!move_parse(&mv, mv_in)) {
+					wprintf(L"!move\n");
+					continue;
+				}
+
+				if (!go_is_move_legal(st, &mv)) {
+					wprintf(L"!illegal\n");
+					continue;
+				}
+
+				// play(move)
+				move_result mv_result = go_play_move(st, &mv);
+				if (mv_result != SUCCESS) {
+					wprintf(L"!play %d\n", mv_result);
+					continue;
+				}
+
+				teresa.observe(&teresa, st, player, &mv);
+				break;
+			}
+			case 'g': {
+				int player_in;
+				result = sscanf(line + 2, "%d", &player_in);
+
+				if (player_in != 1 && player_in != 2) {
+					wprintf(L"!syntax\n");
+					continue;
+				}
+
+				color player = (player_in == 1) ? BLACK : WHITE;
+				if (player != st->nextPlayer) {
+					// TODO Violates GTP: color "should" not be constrained
+					wprintf(L"!player\n");
+					continue;
+				}
+
+				move mv;
+				move_result mv_result = teresa.play(&teresa, st, &mv);
+				if (mv_result != SUCCESS) {
+					wprintf(L"!play %d\n", mv_result);
+					continue;
+				}
+
+				move_print(&mv);
+				wprintf(L"\n");
+				break;
+			}
+			case 'q': {
+				return 0;
+				break;
+			}
+			default:
+				break;
+		}
+	}
+	return 0;
 }
 
 int game_main() {
